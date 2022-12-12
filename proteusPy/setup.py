264044d40cb81a458dc90491b35b03672c8eb98d
Eric Suchanek <suchanek@mac.com>
2022-12-11 20:01:37 -0500

    reorg for packaging

---
+++ proteusPy/setup.py
@@ -0,0 +1,12 @@
+#
+from distutils.core import setup
+
+setup(name='proteusPy',
+      version='0.1',
+      description='proteus structure analysis tools',
+      url='https://github.com/proteusPy/',
+      author='Eric G. Suchanek, PhD',
+      author_email='suchanek@mac.com',
+      license='BSD',
+      packages=['proteusPy'],
+      zip_safe=False)
