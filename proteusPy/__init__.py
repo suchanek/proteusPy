7cb2144a5781ebea5b1c8f808c92019b75b60957
Eric Suchanek <suchanek@mac.com>
2022-12-11 20:01:37 -0500

    reorg for packaging

--- proteusPy/__init__.py
+++ proteusPy/__init__.py
@@ -1,5 +1,17 @@
 #
 #
-# init for bby package
+# init for proteusPy package
+
+from proteusPy import Disulfide
+from proteusPy import turtle3D
+from proteusPy import Residue
+from proteusPy import ProteusPyWarning
+from proteusPy import DisulfideExceptions
+from proteusPy import ProteusGlobals
+
+
+
+
+
 
 
