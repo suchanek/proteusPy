7558d0a7000b6ef080705ee0e27e82e45f682ed0
Eric Suchanek <suchanek@mac.com>
2022-12-12 09:17:57 -0500

    changes and cleanup for package

--- proteusPy/DisulfideGlobals.py
+++ proteusPy/DisulfideGlobals.py
@@ -1,11 +1,9 @@
 
 # global directory for PDB files and the correspondig .pkl models
-PDB_DIR = '/Users/egs/PDB/good/'
-MODEL_DIR = '/Users/egs/pdb/models/'
 SS_PICKLE_FILE = 'PDB_all_ss.pkl'
 SS_DICT_PICKLE_FILE = 'PDB_all_ss_dict.pkl'
 SS_TORSIONS_FILE = 'PDB_SS_torsions.csv'
 PROBLEM_ID_FILE = 'PDB_SS_problems.csv'
-
+SS_ID_FILE = 'ss_ids.txt'
 # Cα Cβ Sγ Χ1 - Χ5 Χ
 
