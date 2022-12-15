b6491735be059cc826e3f86e2b6c4b10430c3034
Eric Suchanek <suchanek@mac.com>
2022-12-15 07:41:37 -0500

    graphing torsions

--- proteusPy/__init__.py
+++ proteusPy/__init__.py
@@ -42,7 +42,7 @@ from proteusPy.turtle3D import ORIENT_BACKBONE, ORIENT_SIDECHAIN
 
 from proteusPy.disulfide import DisulfideList, DisulfideLoader, CysSelect
 from proteusPy.disulfide import name_to_id, todeg, torad, build_torsion_df
-from proteusPy.disulfide import parse_ssbond_header_rec, DownloadDisulfides, DisulfideExtractor
+from proteusPy.disulfide import parse_ssbond_header_rec, DownloadDisulfides, DisulfideExtractor, check_chains
 from proteusPy.DisulfideExceptions import DisulfideIOException, DisulfideConstructionWarning, DisulfideConstructionException
 from proteusPy.residue import build_residue, get_backbone_from_chain, to_alpha, to_carbonyl, to_nitrogen, to_oxygen
 
