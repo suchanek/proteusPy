6c412a608283d019213eb1ad2a65d7ce6bb4cbfb
Eric Suchanek <suchanek@mac.com>
2022-12-23 17:10:31 -0500

    added render_Disulfide

--- programs/DisulfideBondDisplay.py
+++ programs/DisulfideBondDisplay.py
@@ -35,63 +35,6 @@ MODELS = f'{PDB_BASE}models/'
 # location of the compressed Disulfide .pkl files
 # MODELS = f'{PDB_BASE}models/'
 
-PDB_SS = None
-PDB_SS = DisulfideLoader(verbose=True, modeldir=MODELS)
-
-#
-#
-# split out the left/right handed torsions from the dataframe
-#
-# retrieve the torsions dataframe
-
-_cols = ['chi1', 'chi2', 'chi3', 'chi4', 'chi5', 'energy', 'ca_distance']
-
-_SSdf = PDB_SS.getTorsions()
-
-# there are a few structures with bad SSBonds. Their
-# CA distances are > 7.0. We remove them from consideration
-# below
-
-_far = _SSdf['ca_distance'] >= 7.0
-_near = _SSdf['ca_distance'] < 7.0
-_zed = _SSdf['ca_distance'] == 0.0
-
-SS_df_zed = _SSdf[_zed].copy()
-SS_df_Far = _SSdf[_far].copy()
-SS_df = _SSdf[_near].copy()
-
-SS_df = SS_df[_cols].copy()
-
-#print(SS_df.head(5))
-SS_df.describe()
-
-# make two dataframes containing left handed and right handed disulfides
-
-_left = SS_df['chi3'] <= 0.0
-_right = SS_df['chi3'] > 0.0
-
-# left handed and right handed torsion dataframes
-SS_df_Left = SS_df[_left]
-SS_df_Right = SS_df[_right]
-
-# routine creates 2 lists  for left-handed and right-handed disulfides 
-ss_list = PDB_SS.getlist()
-left_handed_SS = DisulfideList([], 'left_handed')
-right_handed_SS = DisulfideList([], 'right_handed')
-
-i = 0
-
-for i in range(0, len(ss_list)):
-    ss = ss_list[i]
-    if ss.chi3 < 0:
-        left_handed_SS.append(ss)
-    else:
-        right_handed_SS.append(ss)
-
-
-print(f'Left Handed: {len(left_handed_SS)}, Right Handed: {len(right_handed_SS)}')
-
-
 # Set the figure sizes and axis limits.
 DPI = 220
 WIDTH = 6.0
@@ -103,16 +46,13 @@ GRIDSIZE = 20
 import pyvista as pv
 from pyvista import Plotter
 
-def main():
-    pl = pv.Plotter(window_size=(2048, 2048),)
-    pl.enable_anti_aliasing('msaa')
-
-    for i in range(2):
-        render_ss(PDB_SS[i], pl)
-    
-    pl.show()
+PDB_SS = None
+PDB_SS = DisulfideLoader(verbose=True, modeldir=MODELS)
 
 if __name__ == '__main__':
-    main()
+    pl = pv.Plotter()
+    pl = render_disulfide_panel(PDB_SS[0])
+    pl.show()
+    exit()
 
 
