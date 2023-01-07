a01bec0beb3a5bf6d49044e4d031e0618d45c38b
Eric Suchanek <suchanek@mac.com>
2023-01-07 16:19:56 -0500

    rendering cleanup, _render is done.

--- programs/DisplaySS.py
+++ programs/DisplaySS.py
@@ -43,23 +43,24 @@ if __name__ == '__main__':
 
     ss.display(style='cpk', single=True)
     ss.display(style='sb', single=True)
-    ss.display(style='pd', single=True) # fix
+    ss.display(style='pd', single=False) # fix
 
-    #ss.screenshot(style='cpk', single=True, fname='cpk3.png', verbose=True)
-    #ss.screenshot(style='sb', single=False, fname='sb3.png', verbose=True)
+    ss.screenshot(style='cpk', single=True, fname='cpk3.png', verbose=True)
+    ss.screenshot(style='sb', single=False, fname='sb3.png', verbose=True)
 
     # get all disulfides for one structure. Make a 
     # DisulfideList object to hold it
     
     ss4yss = DisulfideList([], 'tmp')
     ss4yss = PDB_SS['4yys']
-    #ss4yss.screenshot(style='sb', fname='ss4yss.png')
 
-    #ss4yss.display('cpk')
-    #ss4yss.display('bs')
-    #ss4yss.display('sb')
-    #ss4yss.display('pd')
+    ss4yss.display('cpk')
+    ss4yss.display('bs')
+    ss4yss.display('sb')
+    ss4yss.display('pd')
     ss4yss.display_overlay()
+    ss4yss.screenshot(style='pd', fname='ss4yss.png')
+
     
     sslist = DisulfideList([], 'last8')
     sslist = PDB_SS[:8]
@@ -68,7 +69,7 @@ if __name__ == '__main__':
     sslist.display(style='bs')
     sslist.display(style='pd')
 
-    #sslist.display_overlay()
+    sslist.display_overlay()
 
     #ss1j5h = DisulfideList([], 'ss1j5h')
     #ss1j5h = PDB_SS['1j5h']
