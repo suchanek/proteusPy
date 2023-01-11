eae65650b378252e47f803f073ebd5c53af18aa2
Eric Suchanek <suchanek@mac.com>
2023-01-11 07:57:33 -0500

    tweaks to rendering

--- proteusPy/atoms.py
+++ proteusPy/atoms.py
@@ -32,7 +32,7 @@ Z_RAD_CPK = .8
 ATOM_RADII_CPK = {"N": N_RAD_CPK, "C": C_RAD_CPK, "CA": CA_RAD_CPK, 
                 "O": O_RAD_CPK, "SG": S_RAD_CPK, "S": S_RAD_CPK, 
                 "H": H_RAD_CPK, "CB": C_RAD_CPK, "Z": Z_RAD_CPK}
-ATOM_COLORS = {'O': 'red', 'C': 'grey', 'N': [0.0, 0.0, 1.0], 'S': 'yellow', 'H': 'white', 
+ATOM_COLORS = {'O': 'red', 'C': 'grey', 'N': 'blue', 'S': 'yellow', 'H': 'white', 
             'SG': 'yellow', 'CB': 'grey', 'FE': 'green', 'Z': 'silver'}
 
 BOND_RADIUS = .12
@@ -40,7 +40,7 @@ BOND_COLOR = 'grey'
 FONTSIZE = 10
 BS_SCALE = .25
 SPECULARITY = .7
-SPEC_POWER = 90
+SPEC_POWER = 80
 CAMERA_SCALE = .5
 
 WINFRAME = 512 # single panel width
