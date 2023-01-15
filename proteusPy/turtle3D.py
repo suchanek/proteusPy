3b89381439f716297818d99320fe21686728e3ed
Eric Suchanek <suchanek@mac.com>
2023-01-15 10:19:16 -0500

    added properties to turtle, many changes

--- proteusPy/turtle3D.py
+++ proteusPy/turtle3D.py
@@ -1,4 +1,3 @@
-#
 # Turtle3D.py
 #
 # Implementation of a 3D Turtle in Python.
@@ -69,7 +68,17 @@ class Turtle3D:
         self._recording = False
         self._tape = []
     
+    def new(self, name: str, pos: Vector, head: Vector, left=Vector(0,1,0), 
+            up=Vector(0,0,1), pen='up', recording=False):
+        self.Name = name
+        self.Position = pos
+        self.Heading = head
+        self.Left = left
+        self.Pen = pen
+        self.Recording = recording
+
     def copy_coords(self, source):
+
         '''
         Copy the Position, Heading, Left and Up coordinate system from the input source into self.
         Argument: source: Turtle3D
@@ -96,58 +105,70 @@ class Turtle3D:
         Arguments: None
         Returns: None
         """
-        self._position = numpy.array((0.0, 0.0, 0.0), "d")
-        self._heading = numpy.array((1.0, 0.0, 0.0), "d")
-        self._left = numpy.array((0.0, 1.0, 0.0), "d")
-        self._up = numpy.array((0.0, 0.0, 1.0), "d")
-
-        # expose these as Vectors
-        self.position = Vector(0.0, 0.0, 0.0)
-        self.heading = Vector(1.0, 0.0, 0.0)
-        self.left = Vector(0.0, 1.0, 0.0)
-        self.up = Vector(0.0, 0.0, 1.0)
-
-        self._pen = _UP_
-        self._orientation = -1
-        self._recording = False
-        self._tape = []
+        self.__init__(self)
     
+    @property
     def Orientation(self):
         return self._orientation
     
-    def set_orientation(self, orientation):
-        assert orientation == ORIENT_BACKBONE or orientation == ORIENT_SIDECHAIN, f'Orientation must be {ORIENT_BACKBONE} or {ORIENT_SIDECHAIN}'
+    @Orientation.setter
+    def Orientation(self, orientation):
+        assert orientation == ORIENT_BACKBONE or orientation == ORIENT_SIDECHAIN, \
+         f'Orientation must be {ORIENT_BACKBONE} or {ORIENT_SIDECHAIN}'
         self._orientation = orientation
     
+    @property
     def Pen(self):
         if self._pen == _UP_:
-            return('UP')
+            return('up')
         else:
-            return('Down')
-
-    def PenUp(self):
-        self._pen = _UP_
-
-    def PenDown(self):
-        self._pen = _DOWN_
-
+            return('down')
+    
+    @Pen.setter
+    def Pen(self, pen):
+        if pen == 'up':
+            self._pen = _UP_
+        elif pen == 'down':
+            self._pen = _DOWN_
+        else:
+            self._pen = _DOWN_
+    
+    @property
     def Recording(self):
         return self._recording
 
-    def RecordOn(self):
-        self._recording = True
-
-    def RecordOff(self):
-        self._recording = False
+    @Recording.setter
+    def Recording(self, recording: bool):
+        self._recording = recording
 
     def ResetTape(self):
+        self.Recording(False)
         self._tape = []
 
-    def setPosition(self, x, y=None, z=None):
+    @property 
+    def Position(self):
         """
-        Set the Turtle's Position.
-            X is either a list or tuple.
+        The Turtle's Position
+
+        :return: Position
+        :rtype: Vector
         """
+        return(self.position)
+
+    @Position.setter
+    def Position(self, x, y=None, z=None):
+        """
+        Set's the Turtle's Position
+
+        :param x: X coordinate
+        :type x: float
+        :param y: Y coordinate, defaults to None
+        :type y: float, optional
+        :param z: Z coordinate, defaults to None
+        :type z: float, optional
+        :raises ValueError: Type error
+        """       
+        
         if y is None and z is None:
             # Array, list, tuple...
             if len(x) != 3:
@@ -159,113 +180,31 @@ class Turtle3D:
 
         self.position = Vector(self._position)
         return
-
-    def getPosition(self) -> numpy.array:
-        """
-        Get the Turtle's Position.
-        Return: Turtle's position (Array)
-        """
-        return(self._position)
-
-    def getVPosition(self) -> Vector:
-        """
-        Get the Turtle's Position.
-        Return: Turtle's position (Array)
-        """
-        return(self._position)
     
-    def getName(self):
+    @property 
+    def Heading(self):
         """
-        Get the Turtle's Position.
-        """
-        return(self._name)
-    
-    def setName(self, name):
-        """
-        Set the Turtle'Name.
-        
-        """
-        self._name = name
-    
-    def move(self, distance):
-        """
-        Move the Turtle distance, in direction of Heading
-        """
-        self._position = self._position + self._heading * distance
+        Get the Turtle's Heading
 
-    def roll(self, angle):
+        :return: Heading
+        :rtype: Vector
         """
-        Roll the Turtle about the heading vector angle degrees
+        return(self.heading)
+
+    @Heading.setter
+    def Heading(self, x, y=None, z=None):
         """
-        
-        ang = angle * math.pi / 180.0
-        cosang = numpy.cos(ang)
-        sinang = numpy.sin(ang)
+        Set the turtle's Heading direction vector
 
-        self._up[0] = cosang * self._up[0] - sinang * self._left[0]
-        self._up[1] = cosang * self._up[1] - sinang * self._left[1]
-        self._up[2] = cosang * self._up[2]- sinang * self._left[2]
-
-        self._left[0] = cosang * self._left[0] + sinang * self._up[0]
-        self._left[1] = cosang * self._left[1] + sinang * self._up[1]
-        self._left[2] = cosang * self._left[2] + sinang * self._up[2]
-
-    def yaw(self, angle):
+        :param x: X coordinate
+        :type x: float
+        :param y: Y coordinate, defaults to None
+        :type y: float, optional
+        :param z: Z coordinate, defaults to None
+        :type z: float, optional
+        :raises ValueError: illegal value
         """
-        Yaw the Turtle about the up vector (180 - angle) degrees. This is used when building molecules
-        """
-        
-        ang = ((180 - angle) * math.pi) / 180.0
-        cosang = numpy.cos(ang)
-        sinang = numpy.sin(ang)
 
-        self._heading[0] = cosang * self._heading[0] + sinang * self._left[0]
-        self._heading[1] = cosang * self._heading[1] + sinang * self._left[1]
-        self._heading[2] = cosang * self._heading[2] + sinang * self._left[2]
-
-        self._left[0] = cosang * self._left[0] - sinang * self._heading[0]
-        self._left[1] = cosang * self._left[1] - sinang * self._heading[1]
-        self._left[2] = cosang * self._left[2] - sinang * self._heading[2]
-
-    def turn(self, angle):
-        """
-        Turn the Turtle about the up vector angle degrees.
-        """
-        
-        ang = (angle * math.pi) / 180.0
-
-        cosang = numpy.cos(ang)
-        sinang = numpy.sin(ang)
-
-        self._heading[0] = cosang * self._heading[0] + sinang * self._left[0]
-        self._heading[1] = cosang * self._heading[1] + sinang * self._left[1]
-        self._heading[2] = cosang * self._heading[2] + sinang * self._left[2]
-
-        self._left[0] = cosang * self._left[0] - sinang * self._heading[0]
-        self._left[1] = cosang * self._left[1] - sinang * self._heading[1]
-        self._left[2] = cosang * self._left[2] - sinang * self._heading[2]
-
-    def pitch(self, angle):
-        """
-        pitch the Turtle about the left vector angle degrees
-        """
-        
-        ang = angle * math.pi / 180.0
-        cosang = numpy.cos(ang)
-        sinang = numpy.sin(ang)
-
-        self._heading[0] = self._heading[0] * cosang - self._up[0] * sinang
-        self._heading[1] = self._heading[1] * cosang - self._up[1] * sinang
-        self._heading[2] = self._heading[2] * cosang - self._up[2] * sinang
-
-        self._up[0] = self._up[0] * cosang + self._heading[0] * sinang
-        self._up[1] = self._up[1] * cosang + self._heading[1] * sinang
-        self._up[2] = self._up[2] * cosang + self._heading[2] * sinang
-
-    def _setHeading(self, x, y=None, z=None):
-        """Set the Turtle's Heading.
-            x is either a list or tuple.
-        """
         if y is None and z is None:
             # Array, list, tuple...
             if len(x) != 3:
@@ -274,12 +213,32 @@ class Turtle3D:
         else:
             # Three numbers
             self._heading = numpy.array((x, y, z), "d")
-        self.heading = Vector(self.heading)
+        self.heading = Vector(self._heading)
         return
     
-    def _setLeft(self, x, y=None, z=None):
-        """Set the Turtle's Left.
-            x is either a list or tuple.
+    @property 
+    def Left(self):
+        """
+        Get the Turtle's Left direction vector
+
+        :return: Left
+        :rtype: Vector
+        """
+
+        return(self.left)
+
+    @Left.setter
+    def Left(self, x, y=None, z=None):
+        """
+        Set the turtle's Left direction vector
+
+        :param x: X coordinate
+        :type x: float
+        :param y: Y coordinate, defaults to None
+        :type y: float, optional
+        :param z: Z coordinate, defaults to None
+        :type z: float, optional
+        :raises ValueError: illegal value
         """
         if y is None and z is None:
             # Array, list, tuple...
@@ -292,9 +251,29 @@ class Turtle3D:
         self.left = Vector(self._left)
         return
     
-    def _setUp(self, x, y=None, z=None):
-        """Set the Turtle's Up.
-            x is either a list or tuple.
+    @property
+    def Up(self):
+        """
+        The Turtle's Up direction vector
+
+        :return: Up
+        :rtype: Vector
+        """
+
+        return(self.up)
+
+    @Up.setter
+    def Up(self, x, y=None, z=None):
+        """
+        Set the turtle's Up direction vector
+
+        :param x: X coordinate
+        :type x: float
+        :param y: Y coordinate, defaults to None
+        :type y: float, optional
+        :param z: Z coordinate, defaults to None
+        :type z: float, optional
+        :raises ValueError: illegal value
         """
         if y is None and z is None:
             # Array, list, tuple...
@@ -304,9 +283,134 @@ class Turtle3D:
         else:
             # Three numbers
             self._up = numpy.array((x, y, z), "d")
-        self.up = Vector(self.up)
+        self.up = Vector(self._up)
         return
 
+    def VPosition(self) -> Vector:
+        """
+        Return the Turtle's Position as a Vector
+
+        :return: Position
+        :rtype: Vector
+        """
+        return(self._position)
+    
+    @property
+    def Name(self):
+        """
+        Return the Turtle's Name.
+        """
+        return(self._name)
+    
+    @Name.setter
+    def Name(self, name):
+        """
+        Set the Turtle'Name.
+        
+        """
+        self._name = name
+    
+    def move(self, distance):
+        """
+        Move the Turtle distance (Angstroms), in direction of Heading
+
+        :param distance: Amount to move
+        :type distance: float
+        """
+        self._position = self._position + self._heading * distance
+        self.position = Vector(self._position)
+
+    def roll(self, angle):
+        """
+        Roll the Turtle about the heading vector angle degrees
+
+        :param angle: roll angle, degrees -180 -> 180
+        :type angle: float
+        """
+        
+        ang = angle * math.pi / 180.0
+        cosang = numpy.cos(ang)
+        sinang = numpy.sin(ang)
+
+        self._up[0] = cosang * self._up[0] - sinang * self._left[0]
+        self._up[1] = cosang * self._up[1] - sinang * self._left[1]
+        self._up[2] = cosang * self._up[2]- sinang * self._left[2]
+        self.up = Vector(self._up)
+
+        self._left[0] = cosang * self._left[0] + sinang * self._up[0]
+        self._left[1] = cosang * self._left[1] + sinang * self._up[1]
+        self._left[2] = cosang * self._left[2] + sinang * self._up[2]
+        self.left = Vector(self._left)
+
+    def yaw(self, angle):
+        """
+        Yaw the Turtle about the up vector (180 - angle) degrees. 
+        This is used when building molecules
+
+        :param angle: Yaw angle, degrees -180 -> 180
+        :type angle: float
+        """
+        
+        ang = ((180 - angle) * math.pi) / 180.0
+        cosang = numpy.cos(ang)
+        sinang = numpy.sin(ang)
+
+        self._heading[0] = cosang * self._heading[0] + sinang * self._left[0]
+        self._heading[1] = cosang * self._heading[1] + sinang * self._left[1]
+        self._heading[2] = cosang * self._heading[2] + sinang * self._left[2]
+        self.heading = Vector(self._heading)
+
+        self._left[0] = cosang * self._left[0] - sinang * self._heading[0]
+        self._left[1] = cosang * self._left[1] - sinang * self._heading[1]
+        self._left[2] = cosang * self._left[2] - sinang * self._heading[2]
+        self.left = Vector(self._left)
+
+    def turn(self, angle):
+        """
+        Turn the Turtle about the up vector angle degrees.
+
+        :param angle: Turn angle, degrees
+        :type angle: float
+        """
+        
+        ang = (angle * math.pi) / 180.0
+
+        cosang = numpy.cos(ang)
+        sinang = numpy.sin(ang)
+
+        self._heading[0] = cosang * self._heading[0] + sinang * self._left[0]
+        self._heading[1] = cosang * self._heading[1] + sinang * self._left[1]
+        self._heading[2] = cosang * self._heading[2] + sinang * self._left[2]
+        self.heading = Vector(self._heading)
+
+        self._left[0] = cosang * self._left[0] - sinang * self._heading[0]
+        self._left[1] = cosang * self._left[1] - sinang * self._heading[1]
+        self._left[2] = cosang * self._left[2] - sinang * self._heading[2]
+        self.left = Vector(self._left)
+
+    def pitch(self, angle):
+        """
+        pitch the Turtle about the left vector angle degrees
+
+        :param angle: Pitch angle, degrees -180 -> 180
+        :type angle: float
+        """
+        
+        ang = angle * math.pi / 180.0
+        cosang = numpy.cos(ang)
+        sinang = numpy.sin(ang)
+
+        self._heading[0] = self._heading[0] * cosang - self._up[0] * sinang
+        self._heading[1] = self._heading[1] * cosang - self._up[1] * sinang
+        self._heading[2] = self._heading[2] * cosang - self._up[2] * sinang
+        self.heading = Vector(self._heading)
+
+
+        self._up[0] = self._up[0] * cosang + self._heading[0] * sinang
+        self._up[1] = self._up[1] * cosang + self._heading[1] * sinang
+        self._up[2] = self._up[2] * cosang + self._heading[2] * sinang
+        self.up = Vector(self._up)
+    
     def unit(self, v):
         norm = numpy.linalg.norm(v)
         if norm == 0: 
@@ -316,9 +420,14 @@ class Turtle3D:
     def orient(self, position: numpy.array, heading: numpy.array, left: numpy.array):
         """
         Orients the turtle with Position at p1, Heading at p2 and Left at p3
-        Arguments:
-            position
-        """
+
+        :param position: Position
+        :type position: numpy.array
+        :param heading: Heading direction vector
+        :type heading: numpy.array
+        :param left: Left direction vector
+        :type left: numpy.array
+        """        
 
         self._position = position
 
@@ -340,20 +449,19 @@ class Turtle3D:
         return
 
     def orient_at_residue(self, chain, resnumb, orientation):
-        '''
+        """
         Orient the turtle at the specified residue from the input Chain in
         either orientation 1 or 2.
 
-        Arguments: 
-            turtle: input Turtle3D
-            chain: list of Residues in the model, eg: chain = model['A']
-            resnumb: residue number
-            orientation: 
-            1 - at Ca heading towards Cb with N at the left
+        :param chain: list of Residues in the model, eg: chain = model['A']
+        :type chain: _type_
+        :param resnumb: residue number
+        :type resnumb: int
+        :param orientation: 1 - at Ca heading towards Cb with N at the left or \
             2 - at Ca heading towards C with N at the left
-        Returns: None. Turtle internal state is modified
-        '''
-
+        :type orientation: int
+        """
+        
         assert self._orientation == 1 or self._orientation == 2, f'orient_at_residue() requires Turtle3D to be #1 or #2'
         
         residue = chain[resnumb]
@@ -401,10 +509,10 @@ class Turtle3D:
         
         if orientation == ORIENT_SIDECHAIN:
             self.orient(_ca, _cb, _n)
-            self.set_orientation(ORIENT_SIDECHAIN)
+            self.Orientation(ORIENT_SIDECHAIN)
         elif orientation == ORIENT_BACKBONE:
             self.orient(_ca, _c, _n)
-            self.set_orientation(ORIENT_BACKBONE)
+            self.Orientation(ORIENT_BACKBONE)
         return
 
     def to_local(self, global_vec) -> numpy.array:
@@ -425,18 +533,14 @@ class Turtle3D:
         Returns the Turtle-centered local coordinates for input Global vector (3d)
         """
 
-        newpos = global_vec - self._position
-        dp1 = numpy.dot(self._heading, newpos)
-        dp2 = numpy.dot(self._left, newpos)
-        dp3 = numpy.dot(self._up, newpos)
-
-        return Vector(dp1, dp2, dp3)
+        res = self.to_local(global_vec)
+        return Vector(res)
 
     def to_global(self, local) -> numpy.array:
         """
         Returns the global coordinates for input local vector (3d)
         """
-
+    
         p1 = self._position[0] + self._heading[0] * local[0] + self._left[0] * local[1] + self._up[0] * local[2]
         p2 = self._position[1] + self._heading[1] * local[0] + self._left[1] * local[1] + self._up[1] * local[2]
         p3 = self._position[2] + self._heading[2] * local[0] + self._left[2] * local[1] * self._up[2] * local[2]
@@ -447,16 +551,12 @@ class Turtle3D:
         """
         Returns the global coordinates for input local vector (3d)
         """
-
-        p1 = self._position[0] + self._heading[0] * local[0] + self._left[0] * local[1] + self._up[0] * local[2]
-        p2 = self._position[1] + self._heading[1] * local[0] + self._left[1] * local[1] + self._up[1] * local[2]
-        p3 = self._position[2] + self._heading[2] * local[0] + self._left[2] * local[1] * self._up[2] * local[2]
-
-        return Vector(p1, p2, p3)
+        res = self.to_global(local)
+        return Vector(res)
     
     def __repr__(self):
         """Return Turtle 3D coordinates."""
-        return f"<Turtle: {self._name}\n Position: {self._position},\n Heading: {self._heading} \n Left: {self._left} \n Up: {self._up}\n Orientation: {self._orientation}\n Pen: {self.Pen()} \n Recording: {self._recording}>"
+        return f"<Turtle: {self.Name}\n Position: {self.Position},\n Heading: {self.Heading} \n Left: {self.Left} \n Up: {self.Up}\n Orientation: {self.Orientation}\n Pen: {self.Pen} \n Recording: {self._recording}>"
 
     def bbone_to_schain(self):
         '''
@@ -476,7 +576,7 @@ class Turtle3D:
         self.pitch(180.0)
         self.yaw(110.0)
         self.roll(240.0)
-        self.set_orientation(1) # sets the orientation flag
+        self.Orientation(1) # sets the orientation flag
 
     def schain_to_bbone(self):
         '''
@@ -494,7 +594,7 @@ class Turtle3D:
         self.roll(240.0)
         self.yaw(110.0)
         self.roll(120.0)
-        self.set_orientation(2) # sets the orientation flag
+        self.Orientation(2) # sets the orientation flag
         return
 
 # End of file
