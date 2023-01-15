487b149a96024376447e2704b7e4d745037922bc
Eric Suchanek <suchanek@mac.com>
2023-01-15 16:50:29 -0500

    cleanup, started phi-psi analysis

--- proteusPy/turtle3D.py
+++ proteusPy/turtle3D.py
@@ -18,34 +18,7 @@ _DOWN_ = -1
 _UP_ = 1
 _ORIENTATION_INIT = -1
 
-"""
-    Return a (left multiplying) matrix that rotates p onto q.
-
-    :param p: moving vector
-    :type p: L{Vector}
-
-    :param q: fixed vector
-    :type q: L{Vector}
-
-    :return: rotation matrix that rotates p onto q
-    :rtype: 3x3 Numeric array
-
-    Examples
-    --------
-    >>> from Bio.PDB.vectors import rotmat
-    >>> p, q = Vector(1, 2, 3), Vector(2, 3, 5)
-    >>> r = rotmat(p, q)
-    >>> print(q)
-    <Vector 2.00, 3.00, 5.00>
-    >>> print(p)
-    <Vector 1.00, 2.00, 3.00>
-    >>> p.left_multiply(r)
-    <Vector 1.21, 1.82, 3.03>
-
-    """
-
 # Class Definition begins
-
 class Turtle3D:
     """3D Turtle."""
 
@@ -78,7 +51,6 @@ class Turtle3D:
         self.Recording = recording
 
     def copy_coords(self, source):
-
         '''
         Copy the Position, Heading, Left and Up coordinate system from the input source into self.
         Argument: source: Turtle3D
