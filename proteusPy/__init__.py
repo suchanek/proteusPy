3df6bf836f09b69ad31350a5df9462f4d04f4c9b
Eric Suchanek <suchanek@mac.com>
2023-01-22 21:56:52 -0500

    docstrings

--- proteusPy/__init__.py
+++ proteusPy/__init__.py
@@ -1,3 +1,19 @@
+
+'''
+A package for the modeling and analysis of proteins of known structure.
+With an emphasis on Disulfide Bonds. This package reprises my molecular 
+modeling and analysis program [Proteus](https://doi.org/10.1021/bi00368a023), 
+The package relies on the ``Turtle3D`` class, which implements a 
+three-dimensional 'Turtle'. The turtle implements ``Move``, ``Roll``, 
+``Yaw``, ``Pitch`` and ``Turn`` for movement in a three-dimensional space. 
+The ```DisulfideBond``` class implements the protein structure stabilizing element 
+known as a Disulfide Bond. This class and its underlying methods are being used to 
+perform an analysis of proteins contained in the RCSB protein data bank.
+
+Eric G. Suchanek, PhD.
+suchanek@mac.com
+
+'''
 # Initialization for the proteusPy package
 # Copyright (c) 2023 Eric G. Suchanek, PhD., all rights reserved
 # Subject to the MIT public license.
@@ -13,9 +29,6 @@ import copy
 import time
 import datetime
 import math
-
-from matplotlib import cm
-from numpy import linspace
 import numpy
 
 from Bio.PDB.vectors import calc_dihedral, calc_angle
@@ -34,96 +47,6 @@ from .Disulfide import Download_Disulfides, Extract_Disulfides, Check_chains
 
 from .DisulfideLoader import DisulfideLoader
 from .atoms import *
-
-"""
-.. include:: ./README.md
-"""
-def torad(deg):
-    return(numpy.radians(deg))
-
-def todeg(rad):
-    return(numpy.degrees(rad))
-
-class CysSelect(Select):
-    def accept_residue(self, residue):
-        if residue.get_name() == 'CYS':
-            return True
-        else:
-            return False
-
-
-def dist_squared(a,b):
-    return numpy.sum(numpy.square(numpy.subtract(a,b)))
-
-def distance3d(p1: Vector, p2: Vector):
-    '''
-    Calculate the 3D Euclidean distance for 2 Vector objects
-    
-    :param p1, p2: Vector objects of dimensionality 3
-    :return distance: float Distance between two points
-
-    Example:
-
-    >>> from proteusPy import distance3d
-    >>> from Bio.PDB import Vector
-    >>> p1 = Vector(1, 0, 0)
-    >>> p2 = Vector(0, 1, 0)
-    >>> distance3d(p1,p2)
-    1.4142135623730951
-
-    '''
-
-    _p1 = p1.get_array()
-    _p2 = p2.get_array()
-    if (len(_p1) != 3 or len(_p2) != 3):
-        raise ProteusPyWarning("distance3d() requires vectors of length 3!")
-    d = math.dist(_p1, _p2)
-    return d
-
-
-def cmap_vector(steps):
-    '''
-    Return an RGB array of steps rows using
-    
-    Argument:
-        :param steps: int - number of RGB elements to return
-
-    Returns: 
-        :return: numpy.array [steps][3] array of RGB values.
-
-    Example:
-    >>> import numpy
-    >>> from matplotlib import cm
-    >>> from proteusPy import cmap_vector
-    >>> cmap_vector(12)
-    array([[ 31., 119., 180.],
-       [174., 199., 232.],
-       [255., 187., 120.],
-       [152., 223., 138.],
-       [255., 152., 150.],
-       [197., 176., 213.],
-       [140.,  86.,  75.],
-       [227., 119., 194.],
-       [127., 127., 127.],
-       [188., 189.,  34.],
-       [ 23., 190., 207.],
-       [158., 218., 229.]])
-
-    '''
-
-    rgbcol = numpy.zeros(shape=(steps, 3))
-    norm = linspace(0.0, 1.0, steps)
-
-    # colormap possible values = viridis, jet, spectral
-    rgb_all = cm.jet(norm, bytes=True) 
-    i = 0
-    
-    for rgb in rgb_all:
-        rgbcol[i][0] = rgb[0]
-        rgbcol[i][1] = rgb[1]
-        rgbcol[i][2] = rgb[2]
-        i += 1
-
-    return rgbcol
+from .utility import distance_squared, distance3d, cmap_vector
 
 # end of file
