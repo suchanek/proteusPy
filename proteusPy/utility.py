09b8bd818c402350ceb5b4e1358ea7a73811c3d3
Eric Suchanek <suchanek@mac.com>
2023-02-18 20:46:16 -0500

    docs

--- proteusPy/utility.py
+++ proteusPy/utility.py
@@ -243,7 +243,7 @@ def add_sign_columns(df):
     >>> df = pd.DataFrame(data)
     >>> res_df = add_sign_columns(df)
     >>> print(res_df)
-        ss_id  chi1_s  chi2_s  chi3_s  chi4_s  chi5_s
+       ss_id  chi1_s  chi2_s  chi3_s  chi4_s  chi5_s
     0      1      -1       1      -1       1       1
     1      2       1      -1       1       1      -1
     2      3       1       1       1      -1      -1
@@ -285,7 +285,7 @@ def group_by_sign(df):
     ...                    'energy': [-12.0, -10.0, -15.0, -20.0]})
     >>> grouped = group_by_sign(df)
     >>> grouped
-        chi1_s  chi2_s  chi3_s  chi4_s  chi5_s  ca_distance_mean  ca_distance_std  torsion_length_mean  torsion_length_std  energy_mean  energy_std
+       chi1_s  chi2_s  chi3_s  chi4_s  chi5_s  ca_distance_mean  ca_distance_std  torsion_length_mean  torsion_length_std  energy_mean  energy_std
     0      -1       1      -1       1      -1               3.8              NaN                  2.8                 NaN        -10.0         NaN
     1       1      -1       1      -1      -1               3.3              NaN                  4.4                 NaN        -20.0         NaN
     2       1      -1       1      -1       1               2.5              NaN                  3.0                 NaN        -15.0         NaN
@@ -328,7 +328,7 @@ def Create_classes(df):
     ...    'energy': [-2.3, -2.2, -2.1, -2.0, -1.9]
     ... })
     >>> Create_classes(df)
-        class_id ss_id  count  incidence  percentage
+      class_id ss_id  count  incidence  percentage
     0    00200   [2]      1        0.2        20.0
     1    02020   [5]      1        0.2        20.0
     2    20020   [3]      1        0.2        20.0
@@ -352,6 +352,53 @@ def Create_classes(df):
     grouped['percentage'] = grouped['incidence'].apply(lambda x: 100 * x)
 
     return grouped
+
+def About_proteusPy():
+    """
+    *proteusPy* is a Python package specializing in the modeling and analysis of 
+    proteins of known structure with an emphasis on Disulfide Bonds. This package 
+    reprises my molecular modeling program [Proteus](https://doi.org/10.1021/bi00368a023), 
+    and relies on the [Turtle3D](https://suchanek.github.io/proteusPy/proteusPy/turtle3D.html) 
+    class. The turtle implements the functions `Move`, `Roll`, `Yaw`, `Pitch` 
+    and `Turn` for movement in a three-dimensional space. The 
+    [Disulfide](https://suchanek.github.io/proteusPy/proteusPy/Disulfide.html) class 
+    implements methods to analyze the protein structure stabilizing element known as a 
+    *Disulfide Bond*. This class and its underlying methods are being used to perform a 
+    structural analysis of over 35,800 disulfide-bond containing proteins in the RCSB 
+    protein data bank.
+
+    ### Virtual Environment Installation/Creation
+
+    1. Install Anaconda (<http://anaconda.org>)
+       - Create a new environment using python 3.9
+       - Activate the environment
+    2. Build the environment. At this point it's probably best to clone the repo via github 
+       since it contains all of the notebooks and test programs. Ultimately the distribution 
+       can be used from pyPi as a normal package.
+       - Using pyPi:
+         - python3 -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ proteusPy
+       - From gitHub:
+         - Install git-lfs
+           - https://help.github.com/en/github/managing-large-files/installing-git-large-file-storage
+           - From a shell prompt: git-lfs track "*.csv" "*.pkl" "*.mp4"
+         - git clone https://github.com/suchanek/proteusPy/proteusPy.git
+         - cd into the repo
+         - pip install .
+
+    #### Publications
+    * https://doi.org/10.1021/bi00368a023
+    * https://doi.org/10.1021/bi00368a024
+    * https://doi.org/10.1016/0092-8674(92)90140-8
+    * http://dx.doi.org/10.2174/092986708783330566
+
+    *NB:* This distribution is actively being developed and will be difficult to implement 
+    locally unless the BioPython patch is applied. Also, if you're running on an M-series Mac 
+    then it's important to install Biopython first, since the generic release won't build 
+    on the M1. 2/18/23 -egs
+
+    Eric G. Suchanek, PhD. mailto:suchanek@mac.com
+    """
+    return About_proteusPy.__doc__
 
 if __name__ == "__main__":
     import doctest
