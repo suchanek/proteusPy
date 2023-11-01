102e57eead7487066e79ebe41abd723130e7a5df
Eric Suchanek <suchanek@mac.com>
2023-10-31 20:11:51 -0400

    database interface development

--- programs/DisplaySS2.py
+++ programs/DisplaySS2.py
@@ -10,95 +10,125 @@ from proteusPy.DisulfideLoader import DisulfideLoader, Load_PDB_SS
 from proteusPy.DisulfideList import DisulfideList
 import panel as pn
 
+pn.extension('mathjax')
 pn.extension('vtk', sizing_mode='stretch_width', template='fast')
+
 pn.state.template.param.update(title="RCSB Disulfide Browser")
 
 import pyvista as pv
 from pyvista import set_plot_theme
 
-# the locations below represent the actual location on the dev drive.
-# location for PDB repository
-PDB_BASE = '/Users/egs/PDB/'
-
-# location of cleaned PDB files
-PDB = '/Users/egs/PDB/good/'
-
-# location of the compressed Disulfide .pkl files
-MODELS = f'{PDB_BASE}models/'
-
-global _rcsid
-global _ssidlist
-
 import pyvista as pv
 from proteusPy.Disulfide import *
 
-def SS_DisplayTest(ss: Disulfide):
-    ss.display(style='bs', single=False)
-    ss.display(style='cpk')
-    ss.display(style='sb', single=True)
-    ss.display(style='pd', single=False)
-    ss.screenshot(style='cpk', single=True, fname='cpk3.png', verbose=True)
-    # ss.screenshot(style='sb', single=False, fname='sb3.png', verbose=True)
-    return
+PDB_SS = Load_PDB_SS(verbose=True, subset=False)
 
-def SSlist_DisplayTest(sslist):
-    sslist.display(style='cpk')
-    sslist.display(style='bs')
-    sslist.display(style='sb')
-    sslist.display(style='pd')
-    sslist.display(style='plain')
-    sslist.display_overlay(movie=False, fname='overlay.mp4')
-    #sslist.screenshot(style='sb', fname='sslist.png')
+_rcsid = '2q7q'
+_ssidlist = [
+    '2q7q_75D_140D',
+    '2q7q_81D_113D',
+    '2q7q_88D_171D',
+    '2q7q_90D_138D',
+    '2q7q_91D_135D',
+    '2q7q_98D_129D',
+    '2q7q_130D_161D']
 
-def create_plot(rcs_id="2q7q", ss_id='2q7q_75D_140D', single=True):
+RCSB_list = sorted(PDB_SS.IDList)
+
+def click_plot(event):
+    create_plot()
+
+# Widgets
+
+rcsb_ss_widget = pn.widgets.Select(name="Disulfide", value="2q7q_75D_140D", options=_ssidlist)
+
+#rcsb_ss_widget.param.watch(get_ss_id, 'value')
+
+button = pn.widgets.Button(name='Render', button_type='primary')
+button.on_click(click_plot)
+
+styles_group = pn.widgets.RadioBoxGroup(name='Rending Styles', options=['Split Bonds', 'CPK', 'Ball and Stick'], inline=False)
+single_checkbox = pn.widgets.Checkbox(name='Single View', value=False)
+rcsb_selector_widget = pn.widgets.Select(name="RCSB ID", value="2q7q", options=RCSB_list)
+output_md = pn.pane.Markdown("# Output goes here")
+
+# controls on sidebar
+ss_props = pn.WidgetBox('# Disulfide Selection',
+    rcsb_selector_widget, rcsb_ss_widget,
+).servable(target='sidebar')
+
+ss_styles = pn.WidgetBox('# Rendering Styles',
+    styles_group, single_checkbox, button,
+).servable(target='sidebar')
+
+
+# Callbacks
+def get_ss_idlist(event) -> list:
     global PDB_SS
+    rcs_id = event.new
+    sslist = DisulfideList([],'tmp')
+    sslist = PDB_SS[rcs_id]
+
+    print(f'RCS: {rcs_id}, |{sslist}|')
+    idlist = [ss.name for ss in sslist]
+    rcsb_ss_widget.options = idlist
+
+    return idlist
+
+rcsb_selector_widget.param.watch(get_ss_idlist, 'value')
+
+def get_ss(event) -> Disulfide:
+    global PDB_SS
+    ss_id = event.new
+    ss = Disulfide(PDB_SS[ss_id])
+    return ss
+
+def get_ss_id(event):
+    rcsb_ss_widget.value = event.new
+
+def create_plot(single=True):
+    global PDB_SS
+
+    styles = {"Split Bonds": 'sb', "CPK":'cpk', "Ball and Stick":'bs'}
+
     ss = Disulfide()
     plotter = pv.Plotter()
-
-    _rcsid = rcs_id
-    _ssidlist = [ss.name for ss in PDB_SS[rcs_id]]
+    ss_id = rcsb_ss_widget.value
+    style = styles[styles_group.value]
+    single = single_checkbox.value
+    output_md.object = f'--- Rendering: {ss_id} in style |{style}| single: {single}---'
 
     ss = PDB_SS[ss_id]
-    plotter = ss.plot(single=single)
+    if ss is None:
+        print(f'Cannot find ss_id {ss_id}! Returning!')
+        output_md.object = f'Cannot find ss_id {ss_id}! Returning!'
+        return
+    
+    plotter = ss.plot(single=single, style=style)
     
     # save initial camera properties
     renderer = list(plotter.ren_win.GetRenderers())[0]
     initial_camera = renderer.GetActiveCamera()
+    
     initial_camera_pos = {
         "focalPoint": initial_camera.GetFocalPoint(),
         "position": initial_camera.GetPosition(),
         "viewUp": initial_camera.GetViewUp()
     }
-
+    
     # Panel creation using the VTK Scene created by the plotter pyvista
     orientation_widget = True
     enable_keybindings = True
-    return pn.pane.VTK(
+    vtkpan = pn.pane.VTK(
         plotter.ren_win, margin=0, sizing_mode='stretch_both', orientation_widget=orientation_widget,
         enable_keybindings=enable_keybindings, min_height=600
     )
+    return vtkpan
 
+vtkpan = create_plot()
 
-_rcsid = '2q7q'
-_ssidlist = '2q7q_75D_140D'
+pn.bind(get_ss_idlist, rcs_id=rcsb_selector_widget)
 
-PDB_SS = None
-PDB_SS = Load_PDB_SS(verbose=True, subset=False)
+render_win = pn.Column(vtkpan, output_md)
+render_win.servable(target='main')
 
-
-# one disulfide from the database
-ss = Disulfide()
-ss = PDB_SS[0]
-rcsb_list = sorted(PDB_SS.IDList)
-ss_list = [ss.name for ss in PDB_SS['2q7q']]
-
-rcsb_selector_widget = pn.widgets.Select(name="RCSB ID", value="2q7q", options=rcsb_list)
-rcsb_ss_widget = pn.widgets.Select(name="Disulfide", value="2q7q_75D_140D", options=ss_list)
-bound_plot = pn.bind(create_plot, rcs_id=rcsb_selector_widget, ss_id=rcsb_ss_widget)
-
-interface = pn.Column(rcsb_selector_widget, rcsb_ss_widget)
-dashboard = pn.Column(bound_plot)
-
-
-ss_app = pn.Row(interface, bound_plot)
-ss_app.servable()
