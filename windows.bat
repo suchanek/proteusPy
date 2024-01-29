fa97dde528d141bdabcc89a637d318e6a00151e7
Eric Suchanek <suchanek@mac.com>
2024-01-29 14:57:15 -0500

    windows.bat working

--- windows.bat
+++ windows.bat
@@ -1,8 +1,6 @@
-git clone https://github.com/suchanek/proteusPy
-cd .\proteusPy\
-mamba env create --name proteusPy --file=proteusPy.yml
-conda activate proteusPy
-pip install .
-Jupyter contrib nbextension install --sys-prefix
-python -m ipykernel install --user --name proteusPy --display-name "Python (proteusPy)"
-jupyter nbextension enable --py --sys-prefix widgetsnbextension
+@echo off
+REM git clone https://github.com/suchanek/proteusPy
+REM cd .\proteusPy\
+REM mamba env create --name proteusPy --file=proteusPy.yml -y
+conda activate proteusPy & pip install . & Jupyter contrib nbextension install --sys-prefix & python -m ipykernel install --user --name proteusPy --display-name "Python (proteusPy)" & jupyter nbextension enable --py --sys-prefix widgetsnbextension
+
