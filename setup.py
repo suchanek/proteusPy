1b1afb983da7a8f5eb0928776be7cbb7b2557f66
Eric Suchanek <suchanek@mac.com>
2024-03-16 17:37:55 -0400

    moved to pandas 2.2 by rebuilding .pkl files, 0.93

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-__version__ = "0.92.29"
+__version__ = "UNKNOWN"
+
 version_file = this_directory / "VERSION"
 if version_file.is_file():
     with open(version_file) as f:
