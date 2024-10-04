947571a7eec131be3c96bf8eb3fb7d9bf7745889
Eric Suchanek <suchanek@mac.com>
2024-10-03 22:42:29 -0400

    viewer work

--- programs/viewer.py
+++ programs/viewer.py
@@ -1,18 +1,44 @@
+"""
+Molecular Structure Viewer
+
+This program is a PyQt5-based application for viewing molecular structures with support for
+multiple disulfide bonds and various rendering styles. It uses PyVista for 3D visualization
+and PyQt5 for the graphical user interface. The application allows users to toggle between
+different rendering styles, switch between light and dark themes, and save or export the
+visualized scenes.
+
+Modules:
+    os: Provides a way of using operating system dependent functionality.
+    sys: Provides access to some variables used or maintained by the interpreter.
+    pyvista: A 3D plotting and mesh analysis through a streamlined interface for the Visualization 
+    Toolkit (VTK).
+    PyQt5.QtWidgets: Provides a set of UI elements to create classic desktop-style user interfaces.
+    pyvistaqt: Provides a PyQt5 widget for embedding PyVista plots.
+    proteusPy: Custom module for loading PDB files and getting macOS theme.
+
+Classes:
+    MolecularViewer: Main window class for the molecular structure viewer application.
+
+Functions:
+    main: The main entry point of the application.
+"""
+
 import os
 import sys
 
-os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
+import numpy as np
 import pyvista as pv
+from PyQt5.QtCore import Qt
 
 # pylint: disable=E0611
 from PyQt5.QtWidgets import (
     QAction,
     QApplication,
+    QCheckBox,
     QComboBox,
     QFileDialog,
     QHBoxLayout,
     QMainWindow,
-    QMenuBar,
     QMessageBox,
     QPushButton,
     QVBoxLayout,
@@ -20,7 +46,15 @@ from PyQt5.QtWidgets import (
 )
 from pyvistaqt import QtInteractor
 
-from proteusPy import Load_PDB_SS, get_macos_theme
+from proteusPy import (
+    BOND_RADIUS,
+    DisulfideList,
+    Load_PDB_SS,
+    get_jet_colormap,
+    get_macos_theme,
+)
+
+os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
 
 
 class MolecularViewer(QMainWindow):
@@ -33,7 +67,8 @@ class MolecularViewer(QMainWindow):
         current_ss (object): Currently selected disulfide bond.
         current_style (str): Currently selected rendering style.
         plotter_widget (QtInteractor): PyVista QtInteractor widget for 3D visualization.
-        ss_dict (dict): Dictionary where keys are pdb_id and values are lists of disulfides with that pdb_id.
+        ss_dict (dict): Dictionary where keys are pdb_id and values are lists of disulfides
+        with that pdb_id.
     """
 
     def __init__(self, ss_list, winsize=(1200, 800)):
@@ -60,11 +95,14 @@ class MolecularViewer(QMainWindow):
         self.current_ss = self.ss_list[0]
         self.current_style = "sb"  # Initialize with default style
 
-        # Create a dictionary where keys are pdb_id and values are lists of disulfides with that pdb_id
+        # Create a dictionary where keys are pdb_id and values are lists of disulfides
+        # with that pdb_id
+
         self.ss_dict = {}
         for ss in self.ss_list:
             if ss.pdb_id not in self.ss_dict:
                 self.ss_dict[ss.pdb_id] = []
+
             self.ss_dict[ss.pdb_id].append(ss)
 
         # Create buttons for different rendering styles
@@ -95,6 +133,11 @@ class MolecularViewer(QMainWindow):
         self.button_theme = QPushButton("Toggle Theme")
         self.button_theme.clicked.connect(self.toggle_theme)
 
+        # Create a checkbox for single/multiple display
+        self.checkbox_single = QCheckBox("Single")
+        self.checkbox_single.setChecked(True)
+        self.checkbox_single.stateChanged.connect(self.on_checkbox_single_change)
+
         # Layout for the buttons and dropdown
         button_layout = QVBoxLayout()
         button_layout.addWidget(self.button_cpk)
@@ -105,6 +148,7 @@ class MolecularViewer(QMainWindow):
         button_layout.addWidget(self.pdb_dropdown)  # Add PDB ID dropdown
         button_layout.addWidget(self.dropdown)
         button_layout.addWidget(self.button_theme)  # Add theme toggle button
+        button_layout.addWidget(self.checkbox_single)  # Add single/multiple checkbox
         button_layout.addStretch(1)
 
         # Create a QWidget for the buttons and dropdown
@@ -135,6 +179,9 @@ class MolecularViewer(QMainWindow):
         # Create the menubar
         self.create_menubar()
 
+        # Force a refresh on the disulfide list dropdown
+        self.on_pdb_dropdown_change(0)
+
         # Initial display
         self.display(self.current_style)  # Set a default view with current style
 
@@ -161,6 +208,8 @@ class MolecularViewer(QMainWindow):
         self.dropdown.addItems([ss.name for ss in disulfides])
         if disulfides:
             self.current_ss = disulfides[0]
+            self.current_sslist = DisulfideList(list(disulfides), "sublist")
+            print(f"sslist: {self.current_sslist}")
             self.statusBar().showMessage(f"Displaying: {self.current_ss.name}")
             self.display(self.current_style)
 
@@ -175,12 +224,21 @@ class MolecularViewer(QMainWindow):
         self.statusBar().showMessage(f"Displaying: {self.current_ss.name}")
         self.display(self.current_style)  # Use the current style
 
+    def on_checkbox_single_change(self, state):
+        """
+        Handles the event when the single/multiple checkbox state changes.
+
+        Parameters:
+            state (int): The state of the checkbox (Qt.Checked or Qt.Unchecked).
+        """
+        self.display(self.current_style, single=state == Qt.Checked)
+
     def toggle_theme(self):
         """
         Toggles between light and dark themes for the visualization.
         """
         self.current_theme = "dark" if self.current_theme == "light" else "light"
-        print(f"Toggling theme to: {self.current_theme}")
+        # print(f"Toggling theme to: {self.current_theme}")
 
         self.statusBar().showMessage(
             f"Theme switched to: {self.current_theme.capitalize()}"
@@ -198,12 +256,13 @@ class MolecularViewer(QMainWindow):
         else:
             pv.set_plot_theme("document")
             self.plotter_widget.set_background("white")
-        print(f"Applying theme: {_theme}")
+        # print(f"Applying theme: {_theme}")
 
         # Render the plotter to apply the changes
         self.plotter_widget.render()
 
     def create_menubar(self):
+        """Creates the menubar"""
         menubar = self.menuBar()
 
         # File menu
@@ -220,6 +279,7 @@ class MolecularViewer(QMainWindow):
         file_menu.addAction(export_scene_action)
 
     def save_screenshot(self):
+        """Save the current scene as a screenshot"""
         options = QFileDialog.Options()
         file_path, _ = QFileDialog.getSaveFileName(
             self,
@@ -232,6 +292,7 @@ class MolecularViewer(QMainWindow):
             self.plotter_widget.screenshot(file_path)
 
     def export_scene(self):
+        """Export the current scene as an HTML file"""
         options = QFileDialog.Options()
         file_path, _ = QFileDialog.getSaveFileName(
             self,
@@ -268,7 +329,7 @@ class MolecularViewer(QMainWindow):
         plotter.add_light(light2)
         plotter.add_light(light3)
 
-    def display(self, style, light="Auto", single=True, winsize=(800, 800)):
+    def display(self, style, light="Auto", single=True):
         """
         Renders the current disulfide bond using the specified style.
 
@@ -283,8 +344,12 @@ class MolecularViewer(QMainWindow):
 
         plotter.clear()
 
-        title = f"{self.current_ss.energy:.2f} kcal/mol. Cα: {self.current_ss.ca_distance:.2f} Å Cβ: {self.current_ss.cb_distance:.2f} Å Tors: {self.current_ss.torsion_length:.2f}°"
-
+        title = (
+            f"{self.current_ss.energy:.2f} kcal/mol. "
+            f"Cα: {self.current_ss.ca_distance:.2f} Å "
+            f"Cβ: {self.current_ss.cb_distance:.2f} Å "
+            f"Tors: {self.current_ss.torsion_length:.2f}°"
+        )
         # Determine the theme and set the background color
         _theme = (
             light.lower() if light in ["Light", "Dark"] else get_macos_theme().lower()
@@ -298,7 +363,6 @@ class MolecularViewer(QMainWindow):
 
         # Apply the current theme
         self.current_theme = _theme
-
         self.apply_theme()
 
         self.setWindowTitle(f"{self.current_ss.name} - {title}")
@@ -308,37 +372,102 @@ class MolecularViewer(QMainWindow):
         self.add_custom_lights(plotter)
 
         # Perform the plotting with the specified style
-        try:
+        if single:
             self.current_ss._render(plotter, style=style)
-        except AttributeError as e:
-            QMessageBox.critical(self, "Error", f"Rendering failed: {e}")
-            return
+        else:
+            self.current_sslist._render(plotter, style=style)
 
         # Set perspective projection
         plotter.camera.SetParallelProjection(False)  # False for perspective
-
         plotter.reset_camera()
         plotter.render()
+
+    def display_overlay(
+        self,
+        pl,
+        light="light",
+    ):
+        """
+        Display all disulfides in the list overlaid in stick mode against
+        a common coordinate frames. This allows us to see all of the disulfides
+        at one time in a single view. Colors vary smoothy between bonds.
+
+        :param screenshot: Save a screenshot, defaults to False
+        :param movie: Save a movie, defaults to False
+        :param verbose: Verbosity, defaults to True
+        :param fname: Filename to save for the movie or screenshot, defaults to 'ss_overlay.png'
+        :param light: Background color, defaults to True for White. False for Dark.
+        """
+
+        pid = self.pdb_id
+        ssbonds = self.data
+        tot_ss = len(ssbonds)  # number off ssbonds
+        avg_enrg = self.average_energy
+        avg_dist = self.average_distance
+        resolution = self.resolution
+
+        res = 100
+
+        if tot_ss > 100:
+            res = 60
+        if tot_ss > 200:
+            res = 30
+        if tot_ss > 300:
+            res = 8
+
+        title = f"<{pid}> {resolution:.2f} Å: ({tot_ss} SS), Avg E: {avg_enrg:.2f} kcal/mol, Avg Dist: {avg_dist:.2f} Å"
+
+        if light:
+            pv.set_plot_theme("document")
+        else:
+            pv.set_plot_theme("dark")
+
+        pl.add_axes()
+
+        mycol = np.zeros(shape=(tot_ss, 3))
+        mycol = get_jet_colormap(tot_ss)
+
+        # scale the overlay bond radii down so that we can see the individual elements better
+        # maximum 90% reduction
+
+        brad = BOND_RADIUS if tot_ss < 10 else BOND_RADIUS * 0.75
+        brad = brad if tot_ss < 25 else brad * 0.8
+        brad = brad if tot_ss < 50 else brad * 0.8
+        brad = brad if tot_ss < 100 else brad * 0.6
+
+        # print(f'Brad: {brad}')
+        # pbar = tqdm(range(tot_ss), ncols=PBAR_COLS)
+
+        for i, ss in zip(range(tot_ss), ssbonds):
+            color = [int(mycol[i][0]), int(mycol[i][1]), int(mycol[i][2])]
+            ss._render(
+                pl,
+                style="plain",
+                bondcolor=color,
+                translate=False,
+                bond_radius=brad,
+                res=res,
+            )
+
+        pl.reset_camera()
+        return pl
 
 
 def main():
     """
     The main entry point of the application.
     """
-    try:
-        pdb = Load_PDB_SS(subset=True, verbose=True)
-        ss_list = (
-            pdb.SSList
-        )  # Ensure Load_PDB_SS returns an object with SSList attribute
-    except Exception as e:
-        # Since we are outside the QApplication context, use print for error
-        print(f"Error loading PDB: {e}")
-        sys.exit(1)
 
-    app = QApplication(sys.argv)
-    viewer = MolecularViewer(ss_list)
-    viewer.show()
-    sys.exit(app.exec_())
+    pdb = Load_PDB_SS(subset=True, verbose=True)
+    if pdb is not None:
+        ss_list = sorted(pdb.SSList, key=lambda ss: ss.pdb_id)
+        app = QApplication(sys.argv)
+        viewer = MolecularViewer(ss_list)
+        viewer.show()
+        sys.exit(app.exec_())
+    else:
+        print("Unable to load database!")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
